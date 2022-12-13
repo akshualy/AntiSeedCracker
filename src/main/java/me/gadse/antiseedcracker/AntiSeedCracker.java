c29ba5bc8b16b6fd847d6779e45221ba9189514a
Deko <gadse.deko@gmail.com>
2022-12-13 22:31:49 +0100

    Remove command, randomize seed based on existing

--- src/main/java/me/gadse/antiseedcracker/AntiSeedCracker.java
+++ src/main/java/me/gadse/antiseedcracker/AntiSeedCracker.java
@@ -9,27 +9,24 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.FieldAccessException;
 
-import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+@SuppressWarnings("unused")
 public final class AntiSeedCracker extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        if (!getDataFolder().exists() && !getDataFolder().mkdirs())
-            getLogger().severe("Could not create data folders. Config might not get created.");
-        saveDefaultConfig();
-
-        PluginCommand ascCommand = getCommand("antiseedcracker");
-        if (ascCommand != null) ascCommand.setExecutor(new ASCCommand(this));
-
         ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
         protocolManager.addPacketListener(new PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Play.Server.LOGIN) {
             @Override
             public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
                 try {
-                    packet.getLongs().write(0, getConfig().getLong("replacing-seed"));
+                    packet.getLongs().write(
+                            0, packet.getLongs().read(0) + ThreadLocalRandom.current().nextInt(-9999, 10000)
+                    );
                 } catch (FieldAccessException ex) {
                     ex.printStackTrace();
                 }
@@ -41,7 +38,9 @@ public final class AntiSeedCracker extends JavaPlugin {
             public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
                 try {
-                    packet.getLongs().write(0, getConfig().getLong("replacing-seed"));
+                    packet.getLongs().write(
+                            0, packet.getLongs().read(0) + ThreadLocalRandom.current().nextInt(-9999, 10000)
+                    );
                 } catch (FieldAccessException ex) {
                     ex.printStackTrace();
                 }
